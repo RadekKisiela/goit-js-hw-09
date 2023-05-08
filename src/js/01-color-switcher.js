d59528f41bfc2ab27546a1b3a289801ae6f2e133
RadekKisiela <radek1997_97@o2.pl>
2023-05-08 16:14:06 +0200

    a

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,23 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+let intervalId;
+const startButton=document.querySelector("[data-start]");
+const stopButton=document.querySelector("[data-stop]");
 
-  
+function startColorChange(){
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+}
+function stopColorChange(){
+startButton.disabled = false;
+stopButton.disabled = true;
+clearInterval(intervalId);
+}
+
+function changeBackgroundColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+startButton.addEventListener("click", startColorChange);
+stopButton.addEventListener("click", stopColorChange);
